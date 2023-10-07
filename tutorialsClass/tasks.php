378e45f7430c3ee5c7e1cdb4f029140d1c45ea6b
aytajquseynova <aytajguseyn@gmail.com>
2023-10-07 23:50:59 +0400

    taks

---
+++ tutorialsClass/tasks.php
@@ -0,0 +1,88 @@
+<?php
+
+# Write a program to count 5 to 15 using PHP loop
+// $i = 5 ;
+// while($i <= 15){
+//     echo $i++;
+// }
+
+# Write a program to print “Hello World” using echo
+// echo "Hello World";
+
+
+#Write a program to print “Hello PHP” using variable
+// $greeting = "Hello php";
+// echo $greeting;
+
+#Write a program to print two variables in single echo
+// $var1 = "hello";
+// $var2 = "Aytaj";
+// echo $var1."<br>".$var2;
+
+# Write a program to check student grade based on marks
+// function Marks($score){
+//     if($score > 90){
+//         echo "A";
+//     }
+//     elseif(($score > 80) && ($score <= 90)){
+//         echo "B";
+//     }
+//     elseif(($score > 70) && ($score <= 80)){
+//         echo "C";
+//     }
+//     elseif(($score > 60) && ($score <= 70)){
+//         echo "D";
+//     }
+//     elseif(($score > 50) && ($score <= 60)){
+//         echo "D";
+//     }
+//     else {
+//         echo "Failed";
+//     }
+
+// }
+
+// Marks(49);
+
+# Write a program to show day of the week using switch
+// $weekday = 7;
+// switch ($weekday){  
+//     case 1 : 
+//         echo "monday";
+//         break;
+//     case 2 : 
+//         echo "tuesday";
+//         break;
+//     case 3 : 
+//         echo "wednesday";
+//         break;
+//     case 4 : 
+//         echo "thirsday";
+//         break;
+//     case 5 : 
+//         echo "friday";
+//         break;
+//     case 6 : 
+//         echo "saturday";
+//         break;
+//     case 7 : 
+//         echo "sunday";
+//         break;
+//     default: 
+//         echo "unknown info";
+//         break;                         
+// }
+
+
+#factorial program
+// function factorial($a){
+//     $fac = 1;
+//     for ($i = $a; $i >= 1; $i--){
+//         $fac = $fac * $i;
+//     }
+//     echo $fac;
+// }
+// factorial(5);
+
+
+?>
